804349020b86fe793e8671ef1b91e6b664a24a61
张闯 <cc@zhangchuangdeMacBook-Air-2.local>
2018-09-28 08:24:13 +0800

    暂停

--- src/main/java/typeinfo/toys/ToyTest.java
+++ src/main/java/typeinfo/toys/ToyTest.java
@@ -1,60 +1,72 @@
 //: typeinfo/toys/ToyTest.java
 // Testing class Class.
 package typeinfo.toys;
+
 import static net.mindview.util.Print.*;
 
-interface HasBatteries {}
-interface Waterproof {}
-interface Shoots {}
+interface HasBatteries {
+}
+
+interface Waterproof {
+}
+
+interface Shoots {
+}
 
 class Toy {
-  // Comment out the following default constructor
-  // to see NoSuchMethodError from (*1*)
-  Toy() {}
-  Toy(int i) {}
+    // Comment out the following default constructor
+    // to see NoSuchMethodError from (*1*)
+    Toy() {
+    }
+
+    Toy(int i) {
+    }
 }
 
 class FancyToy extends Toy
-implements HasBatteries, Waterproof, Shoots {
-  FancyToy() { super(1); }
+        implements HasBatteries, Waterproof, Shoots {
+    FancyToy() {
+        super(1);
+    }
 }
 
 public class ToyTest {
-  static void printInfo(Class cc) {
-    print("Class name: " + cc.getName() +
-      " is interface? [" + cc.isInterface() + "]");
-    print("Simple name: " + cc.getSimpleName());
-    print("Canonical name : " + cc.getCanonicalName());
-  }
-  public static void main(String[] args) {
-    Class c = null;
-    try {
-      c = Class.forName("typeinfo.toys.FancyToy");
-    } catch(ClassNotFoundException e) {
-      print("Can't find FancyToy");
-      System.exit(1);
+    static void printInfo(Class cc) {
+        print("Class name: " + cc.getName() +
+                " is interface? [" + cc.isInterface() + "]");
+        print("Simple name: " + cc.getSimpleName());
+        print("Canonical name : " + cc.getCanonicalName());
     }
-    printInfo(c);	
-    for(Class face : c.getInterfaces())
-      printInfo(face);
-    Class up = c.getSuperclass();
-    Object obj = null;
-    try {
-      // Requires default constructor:
-      obj = up.newInstance();
-      //up只是一个指向某个Class对象的引用，不具有任何类型信息。但是依然能够通过newInstance()创建
-      //出一个实例，newInstance()的返回值是Object类型，但这里obj引用实际指向的是Toy对象。
-      //程序员需要通过自己的代码上下文判断出这里实际实例化出来的是Toy对象，并可以对Object的引用进行向下转型为Toy引用。
 
-    } catch(InstantiationException e) {
-      print("Cannot instantiate");
-      System.exit(1);
-    } catch(IllegalAccessException e) {
-      print("Cannot access");
-      System.exit(1);
+    public static void main(String[] args) {
+        Class c = null;
+        try {
+            c = Class.forName("typeinfo.toys.FancyToy");
+        } catch (ClassNotFoundException e) {
+            print("Can't find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+        for (Class face : c.getInterfaces())
+            printInfo(face);
+        Class up = c.getSuperclass();
+        Object obj = null;
+        try {
+            // Requires default constructor:
+            obj = up.newInstance();
+            //up只是一个指向某个Class对象的引用，不具有任何类型信息。但是依然能够通过newInstance()创建
+            //出一个实例，newInstance()的返回值是Object类型，但这里obj引用实际指向的是Toy对象。
+            //程序员需要通过自己的代码上下文判断出这里实际实例化出来的是Toy对象，并可以对Object的引用进行向下转型为Toy引用。
+
+        } catch (InstantiationException e) {
+            print("Cannot instantiate");
+            System.exit(1);
+        } catch (IllegalAccessException e) {
+            print("Cannot access");
+            System.exit(1);
+        }
+        printInfo(obj.getClass());
     }
-    printInfo(obj.getClass());
-  }
 } /* Output:
 Class name: typeinfo.toys.FancyToy is interface? [false]
 Simple name: FancyToy
